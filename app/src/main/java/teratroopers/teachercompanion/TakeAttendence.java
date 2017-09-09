a644598a332115bec42dad050c1a54d6ac36788b
pvinusha <vinushareddy1997@gmail.com>
2017-09-09 10:21:29 +0530

    Initial commit

---
+++ app/src/main/java/teratroopers/teachercompanion/TakeAttendence.java
@@ -0,0 +1,13 @@
+package teratroopers.teachercompanion;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TakeAttendence extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_take_attendence);
+    }
+}
