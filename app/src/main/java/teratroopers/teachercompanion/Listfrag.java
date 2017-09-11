39fe5d2a8cb1e810efb42d10b2e137e82cd34b6d
pvinusha <vinushareddy1997@gmail.com>
2017-09-11 15:17:19 +0530

    Code of Tera Troopers Repositry added

---
+++ app/src/main/java/teratroopers/teachercompanion/Listfrag.java
@@ -0,0 +1,118 @@
+package teratroopers.teachercompanion;
+
+import  android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class Listfrag extends Fragment implements View.OnClickListener {
+    // TODO: Rename parameter arguments, choose names that match
+    View view;
+    mydbhelper mydb;
+
+    Button b1;
+    LinearLayout linearLayout;
+    int i;
+    String cname;
+    String str;
+    public Listfrag() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        view =inflater.inflate(R.layout.fragment_listfrag, container, false);
+        this.mydb = new mydbhelper((Context)this.getActivity());
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        linearLayout=(LinearLayout)view.findViewById(R.id.linearlayout);
+        createbuttons();
+    }
+
+    //TODO create new background for buttons with sharper arrow
+    public void createbuttons() {
+
+        LinearLayout.LayoutParams lp1 = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,150);
+        LinearLayout.LayoutParams lp2 = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 5);
+        lp1.setMargins(20, 20, 20, 10); //ltrd
+        final Cursor res=mydb.getalldata();
+        while (res.moveToNext()) {
+            i=0;
+            cname=res.getString(0);
+            Log.i("name",cname);
+
+            b1 = new Button(getActivity());
+            b1.setText(res.getString(0));
+            b1.setTag(res.getString(0));
+            b1.setElevation(3.8f);
+            b1.setLayoutParams(lp1);
+            b1.setGravity(Gravity.CENTER);
+            linearLayout.addView(b1);
+            //here
+
+          /*  ImageView divider1 = new ImageView(getActivity());
+
+            lp2.setMargins(20, 0, 20, 0);
+            divider1.setLayoutParams(lp2);
+            divider1.setElevation(0.5f);
+            divider1.setBackgroundColor(Color.DKGRAY);
+
+            linearLayout.addView(divider1);
+            */
+
+            //to here
+            b1.setOnClickListener(Listfrag.this);
+            b1.setOnLongClickListener(
+                    new View.OnLongClickListener() {
+                        @Override
+                        public boolean onLongClick(View view) {
+                            str=view.getTag().toString();
+                            new AlertDialog.Builder(getActivity())
+                                    .setTitle("Do you want to delete the class?")
+                                    .setMessage("You can not undo the action")
+                                    .setIcon(android.R.drawable.ic_dialog_alert)
+                                    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+
+                                        public void onClick(DialogInterface dialog, int whichButton) {
+                                            mydb.deleteclass(str);
+                                        }})
+                                    .setNegativeButton(android.R.string.no, null).show();
+                            return true;
+                        }
+
+                    }
+
+            );
+
+        }
+
+    }
+
+    public void onClick(View v) {
+        String str=v.getTag().toString();
+        //Toast.makeText(getActivity(),str+" clicked",Toast.LENGTH_SHORT).show();
+        Intent intent=new Intent("teratroopers.teachercompanion.TakeAttendence");
+        intent.putExtra("name",str);
+        startActivity(intent);
+    }
+
+}
+
+
--- app/src/main/java/teratroopers/teachercompanion/addclass.java
+++ app/src/main/java/teratroopers/teachercompanion/addclass.java
@@ -1,13 +1,137 @@
 package teratroopers.teachercompanion;
 
+import android.database.Cursor;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class addclass extends AppCompatActivity {
+    mydbhelper mydb;
+    EditText a1,a2,a3;
+    Button viw;
+    TextView tv;
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addclass);
+        mydb =new mydbhelper(this);
+        a1 = (EditText) findViewById(R.id.editText);
+        a2 = (EditText) findViewById(R.id.editText2);
+        a3 = (EditText) findViewById(R.id.editText3);
+        viw=(Button)findViewById(R.id.button6);
+        fab=(FloatingActionButton)findViewById(R.id.floatingActionButton);
+        confirmfab();
+        viewall();
+        //backbtn();
+
     }
+    public void confirmfab()
+    {
+
+        fab.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        try {
+                            int sr, er;
+                            String cname;
+                            sr = Integer.parseInt(a2.getText().toString());
+                            er = Integer.parseInt(a3.getText().toString());
+                            cname=a1.getText().toString();
+
+                            if (sr < er) {
+                                boolean isInserted=mydb.dbname(cname,sr,er);
+                                // boolean isInserted = mydb.insertData(cname, sr, er);
+                                if (isInserted == true) {
+                                    Toast.makeText(addclass.this, "Data saved successfully", Toast.LENGTH_SHORT).show();
+                                    a1.setText("");
+                                    a2.setText("");
+                                    a3.setText("");
+                                    tv=(TextView)findViewById(R.id.classaddtext);
+                                    final Handler handler = new Handler();
+                                    handler.postDelayed(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            // Do something after 5s = 5000ms
+                                            tv.animate().alpha(1).setDuration(2000);
+                                        }
+                                    }, 3000);
+
+                                    handler.postDelayed(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            // Do something after 5s = 5000ms
+                                            tv.animate().alpha(0).setDuration(2000);
+                                        }
+                                    }, 7500);
+
+                                } else {
+                                    Toast.makeText(addclass.this, "internal error occurred! please create class with another name",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                            else {
+                                Toast.makeText(addclass.this, "Starting roll no should be less than Ending roll no", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        catch (Exception e) {
+                            Toast.makeText(addclass.this, "Enter data in all fields", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+        );
+    }
+    public void viewall(){
+
+        viw.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Cursor res = mydb.getalldata();
+                        if (res.getCount() == 0) {
+                            //show message
+                            showmessage("Error", "Nothing found");
+                            return;
+                        }
+                        StringBuffer buffer = new StringBuffer();
+                        while (res.moveToNext()) {
+                            buffer.append("ClassName :" + res.getString(0) + "\n");
+                            buffer.append("Starting Roll :" + res.getString(1) + "\n");
+                            buffer.append("Ending Roll :" + res.getString(2) + "\n");
+
+                        }
+                        //show all data
+                        showmessage("Data", buffer.toString());
+                    }
+                }
+        );
+    }
+    public void showmessage(String title,String Message) {
+        AlertDialog.Builder builder = new  AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(Message);
+        builder.show();
+
+    }
+
+   /* public void backbtn(){
+        btn.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        finish();
+                    }
+                }
+        );
+    }*/
 }
