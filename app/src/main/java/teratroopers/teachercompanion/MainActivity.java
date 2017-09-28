610626c7831685048d350754afbcfbe910fb1651
anilchoudary <anilakku2@gmail.com>
2017-09-29 00:37:48 +0530

    Status & Navigation Bar's are Removed

--- app/src/main/java/teratroopers/teachercompanion/MainActivity.java
+++ app/src/main/java/teratroopers/teachercompanion/MainActivity.java
@@ -5,6 +5,8 @@ import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +19,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
         OnClickButtonListener();
         OnClickListListener();
