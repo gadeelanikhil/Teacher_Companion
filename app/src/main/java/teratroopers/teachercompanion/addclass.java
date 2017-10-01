0dfbc1f07c11ee263c419a0d4c174ac0d90af4b6
anilchoudary <anilakku2@gmail.com>
2017-10-01 17:49:29 +0530

    Snack Bar Code Added

--- app/src/main/java/teratroopers/teachercompanion/addclass.java
+++ app/src/main/java/teratroopers/teachercompanion/addclass.java
@@ -3,6 +3,7 @@ package teratroopers.teachercompanion;
 import android.database.Cursor;
 import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,6 +12,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
 
 public class addclass extends AppCompatActivity {
     mydbhelper mydb;
@@ -50,7 +52,19 @@ public class addclass extends AppCompatActivity {
                                 boolean isInserted=mydb.dbname(cname,sr,er);
                                 // boolean isInserted = mydb.insertData(cname, sr, er);
                                 if (isInserted == true) {
-                                    Toast.makeText(addclass.this, "Data saved successfully", Toast.LENGTH_SHORT).show();
+                                   Toast.makeText(addclass.this, "Data saved successfully", Toast.LENGTH_SHORT).show();
+                                   /* Snackbar mySnackbar = Snackbar.make(findViewById(R.id.addclass),
+                                            "Class Added", Snackbar.LENGTH_SHORT);
+                                   // mySnackbar.setAction("UNDO", new MyUndoListener());//
+                                    mySnackbar.show();
+                                  /*  public class MyUndoListener implements View.OnClickListener{
+
+                                        @Override
+                                        public void onClick(View v) {
+
+                                            // Code to undo the user's last action
+                                        }
+                                    }*/
                                     a1.setText("");
                                     a2.setText("");
                                     a3.setText("");
@@ -75,13 +89,24 @@ public class addclass extends AppCompatActivity {
                                 } else {
                                     Toast.makeText(addclass.this, "internal error occurred! please create class with another name",
                                             Toast.LENGTH_SHORT).show();
+                                  /*  Snackbar mineSnackBar = Snackbar.make(findViewById(R.id.addclass),
+                                            "Something went wrong! Please Create class with another name", Snackbar.LENGTH_SHORT);
+                                    mineSnackBar.show();*/
+
                                 }
                             }
                             else {
-                                Toast.makeText(addclass.this, "Starting roll no should be less than Ending roll no", Toast.LENGTH_SHORT).show();
+                               Toast.makeText(addclass.this, "Start Roll_no should be less than End Roll_no", Toast.LENGTH_SHORT).show();
+
+                               /* Snackbar minSnackBar = Snackbar.make(findViewById(R.id.addclass),
+                                        "Start Roll_no should be less than End Roll_no", Snackbar.LENGTH_SHORT);
+                                minSnackBar.show();*/
                             }
                         }
                         catch (Exception e) {
+                           /* Snackbar miSnackBar = Snackbar.make(findViewById(R.id.addclass),
+                                    "Something is wrong! Please try again", Snackbar.LENGTH_SHORT);
+                            miSnackBar.show();*/
                             Toast.makeText(addclass.this, "Enter data in all fields", Toast.LENGTH_SHORT).show();
                         }
                     }
