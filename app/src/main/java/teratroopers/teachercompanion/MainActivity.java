05e390eecb680fbbf556f4f611d403e3258993d4
anilchoudary <anilakku2@gmail.com>
2017-10-01 16:46:45 +0530

    Full Screen Mode Disabled

--- app/src/main/java/teratroopers/teachercompanion/MainActivity.java
+++ app/src/main/java/teratroopers/teachercompanion/MainActivity.java
@@ -17,9 +17,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+       /* requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);*/
         setContentView(R.layout.activity_main);
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT);
 
